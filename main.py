a6b9849ba174bcb4906ff9f4fe204a0f4a2be70d
amitbaskin <amit.baskin@gmail.com>
2021-09-29 14:45:15 +0300

    parse all files

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import urllib.request
 import json
+import os
+import re
 
 KEYS = ['iPad', 'iPhone', 'Android', 'Windows']
 URL = 'http://firefire.cyber.org.il/logs/access_130101.zip'
+HTML = "canvasjs-3/my.html"
+PARENT = "http://firefire.cyber.org.il/logs/"
+CODE = 'unicode_escape'
+PATTERN = "access_[0-9]+.zip"
 
 PART_1 = """
 <!DOCTYPE HTML>
@@ -47,42 +53,41 @@ chart.render();
 """
 
 
-def get_lines(url_path):
-    with urllib.request.urlopen(url_path) as f:
-        return f.readlines()
-
-
 def get_dict(keys):
     return {k: 0 for k in keys}
 
 
-def parse_file(lines, plat_dict):
+def parse_file(url_path, plat_dict, counter):
+    print(url_path)
+    with urllib.request.urlopen(url_path) as f:
+        lines = f.readlines()
+    if not lines:
+        return counter
+    counter += len(lines)
     for line in lines:
-        line_s = line.decode('unicode_escape')
+        line_s = str(line)
         for k in plat_dict:
             if k in line_s:
                 plat_dict[k] += 1
                 continue
+    return counter
+
+
+def get_final_dict(plat_dict, counter):
     for k in plat_dict:
-        plat_dict[k] /= len(lines)
+        plat_dict[k] /= counter
         plat_dict[k] = round(plat_dict[k] * 100, 2)
-
-
-def get_percents(keys, url):
-    plat_dict = get_dict(keys)
-    lines = get_lines(url)
-    parse_file(lines, plat_dict)
     return plat_dict
 
 
-def print_percent(plat_dict):
+def print_percents(plat_dict):
     for k in plat_dict:
         print(f"{k} - {plat_dict[k]}%")
     return plat_dict
 
 
-def write_html(plat_dict):
-    with open("canvasjs-3/my.html", "w") as f:
+def write_html(plat_dict, path):
+    with open(path, "w") as f:
         f.write(PART_1 + '\n')
         for k in plat_dict:
             f.write('{ ')
@@ -91,7 +96,30 @@ def write_html(plat_dict):
         f.write(PART_2)
 
 
+def run_html():
+    os.system(f"open {HTML}")
+
+
+def write_json(plat_dict):
+    with open("my.json", "w") as f:
+        f.write(json.dumps(plat_dict))
+
+
+def parse_all_files(parent):
+    counter = 0
+    plat_dict = get_dict(KEYS)
+    with urllib.request.urlopen(parent) as f:
+        lines = f.readlines()
+        for line in lines:
+            s = re.search(PATTERN, str(line))
+            if s:
+                counter = parse_file(PARENT + s.group(), plat_dict, counter)
+                print(counter)
+                print(plat_dict)
+    plat_dict = get_final_dict(plat_dict, counter)
+    write_html(plat_dict, HTML)
+    run_html()
+
+
 if __name__ == '__main__':
-    # with open("my.json", "w") as f:
-    #     f.write(json.dumps(get_percents(KEYS, URL)))
-    write_html(get_percents(KEYS, URL))
+    parse_all_files(PARENT)
